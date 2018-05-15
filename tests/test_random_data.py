be67fd4fdefc391eea3daa5b762e98936970ef65
Lxinyuelxy <836032851@qq.com>
2018-05-15 17:25:02 +0800

    modify test

--- tests/test_random_data.py
+++ tests/test_random_data.py
@@ -1,16 +1,8 @@
-import numpy as np
 from sklearn.datasets import make_multilabel_classification
 from sklearn.model_selection import train_test_split
-from sklearn.multiclass import OneVsRestClassifier
-from sklearn.svm import SVC
 from mllearn.problem_transform import BinaryRelevance
-from mllearn.problem_transform import CalibratedLabelRanking
-from mllearn.problem_transform import ClassifierChain
-from mllearn.problem_transform import RandomKLabelsets
-from mllearn.alg_adapt import MLKNN
-from mllearn.alg_adapt import MLDecisionTree
 from mllearn.metrics import subset_acc
-from mllearn.metrics import hamming_loss
+from mllearn.metrics import hamming_loss as hamming_loss
 from mllearn.metrics import accuracy
 from mllearn.metrics import precision
 from mllearn.metrics import recall
@@ -25,36 +17,6 @@ X, y = make_multilabel_classification(n_samples=700,
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
 classif = BinaryRelevance()
-classif.fit(X_train, y_train)
-predictions = classif.predict(X_test)
-print('The BinaryRelevance is %f' % hamming_loss(y_test, predictions))
-
-classif = ClassifierChain()
-classif.fit(X_train, y_train)
-predictions = classif.predict(X_test)
-print('The Classifier Chain Result is %f' % hamming_loss(y_test, predictions))
-
-classif = CalibratedLabelRanking()
-classif.fit(X_train, y_train)
-predictions = classif.predict(X_test)
-print('The CalibratedLabelRanking Result is %f' % hamming_loss(y_test, predictions))
-
-classif = RandomKLabelsets()
-classif.fit(X_train, y_train)
-predictions = classif.predict(X_test)
-print('The Random K-Labelsets Result is %f' % hamming_loss(y_test, predictions))
-
-classif = MLKNN()
-classif.fit(X_train, y_train)
-predictions = classif.predict(X_test)
-print('The MLKNN is %f' % hamming_loss(y_test, predictions))
-
-# classif = MLDecisionTree()
-# classif.fit(X_train, y_train)
-# predictions = classif.predict(X_test)
-# print('The MLDecisionTree Result is %f' % hamming_loss(y_test, predictions))
-
-classif = OneVsRestClassifier(SVC(kernel='linear'))
 classif.fit(X_train, y_train)
 predictions = classif.predict(X_test)
 print('The subset_acc Result is %f' % subset_acc(y_test, predictions))
